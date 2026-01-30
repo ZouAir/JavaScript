489ecd30f34a7fbd717b0a2fe0a6a8644a8238e0
Zouhair REGHAI <reghaizouhair@gmail.com>
2026-01-30 16:14:32 +0100

    Add files via upload

---
+++ cours.js
@@ -0,0 +1,30 @@
+// Itération:
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+//**********************************************
+
+// Table de multiplication:
+// for (let i = 1; i <= 10; i++){
+//     console.log("2 x " + i + " = " + (2 * i));
+// }
+//***********************************************
+
+let fruits = ["pomme", "banane", "orange", "kiwi"];
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// fruits[1] = "fraise";
+// console.log(fruits[1]); 
+// console.log(fruits.length); //3
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+//*********************************
+let nombres = [10, 20, 30, 40];
+for (i = 0; i <= fruits.length; i++);
+console.log(nombres[i]);
+
+
---
+++ formulaire.js
@@ -0,0 +1,24 @@
+document.querySelector("#form input[type=submit]").addEventListener("click", (event) => {
+    document.querySelectorAll("#form div.error").forEach((div) => {
+        div.classList.remove("error");
+        div.querySelector(".error-message").remove();
+    });
+    let submit = true;
+    document.querySelectorAll("#form input[required], #form select[required], #form textarea[required]").forEach((input) => {
+    //document.querySelectorAll("#form [required]") : façon plus synthétique
+        if (input.value.trim().length == 0) {
+            submit = false;
+            input.closest("div").classList.add("error");
+            let div = document.createElement("div");
+            div.innerText = "Champs obligatoire";
+            div.classList.add("error-message");
+            input.closest("div").append(div);
+            errors[] = "champs obligatoire";
+        }
+    });
+    
+    if (!submit) {
+        event.preventDefault();
+        console.log("Oups!");
+    }
+});
